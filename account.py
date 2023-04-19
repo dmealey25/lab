569996d0584c63166af8e1f1cae815bf33f0b3e8
dmealey25 <131289775+dmealey25@users.noreply.github.com>
2023-04-19 12:10:02 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,24 +1,51 @@
 class Account:
-    def __init__(self, name):
+        """
+        A class representing an account for a person
+        """
+        
+    def __init__(self, name: str) -> None:
+        """
+        Create the Account object with the given name and a balance of 0.
+        :name: The account name.
+        :balance: The accounts default balance
+        """
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """
+        Deposit the specified amount into the account.
+        :amount: The amount to be deposited.
+        :bool: True if the deposit transaction is successful, False otherwise.
+        """
         if amount > 0:
-            self.__account_balance += amount
+            self.__balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        """
+        Withdraw the specified amount from the account.
+        :amount: The amount to be withdrawn.
+        :bool: True if the withdrawal transaction is successful, False otherwise.
+        """
         if amount > 0 and amount <= self.__balance:
-            self.__account_balance -= amount
+            self.__balance -= amount
             return True
         else:
             return False
 
-    def get_balance(self):
-        return self.__account_balance
+    def get_balance(self) -> float:
+        """
+        Get the current account balance.
+        :balance: The current account balance.
+        """
+        return self.__balance
 
-    def get_name(self):
+    def get_account_name(self) -> str:
+        """
+        Get the account name.
+        :name: The account name.
+        """
         return self.__account_name
