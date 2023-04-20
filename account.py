c809a1632ea1e63ee17dc791f11b32ec4c7bb5e4
dmealey25 <131289775+dmealey25@users.noreply.github.com>
2023-04-20 10:04:20 -0500

    Update account.py

--- account.py
+++ account.py
@@ -11,7 +11,7 @@ class Account:
             return False
 
     def withdraw(self, amount):
-        if amount > 0 and amount <= self.__balance:
+        if amount > 0 and amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
